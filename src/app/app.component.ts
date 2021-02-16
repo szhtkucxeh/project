d5405d9005e8470d620c2ac84478a5e499f1764f
szhtkucxeh <76559782+szhtkucxeh@users.noreply.github.com>
2021-02-16 20:24:35 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+
+})
+
+export class AppComponent {
+[x: string]: any;
+
+ 
+ 
+    
+    
+
+coins=[{model:"aa",price:22},
+       {model:"bb",price:112}];
+} 
